48a8550881099cac8dae315f1a335205cb06a460
Vaibhav1907 <139456373+Vaibhav1907@users.noreply.github.com>
2023-07-16 09:41:45 +0530

    Update cube.py

--- cube.py
+++ cube.py
@@ -1,13 +1,16 @@
-#n = int(input("Enter a number: "))
+# Q1
+# n = int(input("Enter a number: "))
 
 #for i in range(1, n+1):
   #  print(i**3)
   
+#q2
 #numbers=[10,7,32,45,2,456,12,33,11,57,78,98,99,100,22,27,89,44,5,1,20,23,42,44,88,980,67,821]
 #print("odd number are: ")
 #for i in  numbers:
    #if i%2!=0:
     #   print(i)
+
 
 #numbers= [456, 22, 33, 11, 57, 66, 78, 98, 99, 100, 22, 3, 27, 89, 44, 5, 1, 20, 213, 42, 44, 88, 980, 67, 66, 77] 
 #print("odd number are:")
